efb64bcce4c68483f6148e63ab2244fa00f313bb
francesco <francesco.vigolungo@wi-tek.it>
2017-10-13 11:39:50 +0200

    10 ottobre 16:10 - Aggiunta android GCM test

--- push_notification_gcm/public/config.js
+++
@@ -1,4 +0,0 @@
-window.GoogleSamples = window.GoogleSamples || {};
-window.GoogleSamples.Config = window.GoogleSamples.Config || {
-        gcmAPIKey: 'AAAASPdmVu8:APA91bGa6lgLBHOIPJuDqLvR95-DEURQotJlsRihGdZTR88Jnnvuh8e02UtY0U44xmZQXsEGRvJNSS4DUcoDls5T1klmpdGWDWmeOAdR7FktVaYB1EEkkAbkHsa9xdp0udBCOBqNUlFu'
-    };
